0ee634d15b99468ab62928c317fbe272dc1eeae2
Christian Engen <chris.a.engen@gmail.com>
2022-08-02 10:19:11 +0200

    add I kicks

--- src/srs.rs
+++ src/srs.rs
@@ -28,8 +28,8 @@ fn mat3_counter_clockwise_rot(m_a: &Mat3) -> Mat3 {
 fn mat4_clockwise_rot(m_a: &Mat4) -> Mat4 {
     let col = m_a.to_cols_array();
     let m_b = Mat4::from_cols_array(&[
-        col[12], col[13], col[14], col[15], col[8], col[9], col[10], col[11], col[4], col[5],
-        col[6], col[7], col[0], col[1], col[2], col[3],
+        col[3], col[7], col[11], col[15], col[2], col[6], col[10], col[14], col[1], col[5], col[9],
+        col[13], col[0], col[4], col[8], col[12],
     ]);
 
     m_b
@@ -38,8 +38,24 @@ fn mat4_clockwise_rot(m_a: &Mat4) -> Mat4 {
 pub fn rotate(tetromino: &mut Tetromino, placed: &Vec<Option<Block>>) {
     match tetromino.kind {
         TetrominoType::I => {
-            tetromino.mat4 = tetromino.mat4.transpose();
-            tetromino.rot_index = (tetromino.rot_index + 1) % 4;
+            let mut new_tetromino = tetromino.clone();
+            new_tetromino.mat4 = mat4_clockwise_rot(&tetromino.mat4);
+            if can_translate(&new_tetromino, placed, &new_tetromino.pos) {
+                tetromino.mat4 = new_tetromino.mat4;
+                tetromino.rot_index = (tetromino.rot_index + 1) % 4;
+            } else {
+                let res = mat3_super_kick(&I_KICKS, &new_tetromino, placed);
+                match res {
+                    Ok(new_pos) => {
+                        tetromino.mat4 = new_tetromino.mat4;
+                        tetromino.pos = new_pos;
+                        tetromino.rot_index = (tetromino.rot_index + 1) % 4;
+                    }
+                    Err(str) => {
+                        debug!("{}", str);
+                    }
+                }
+            }
         }
         TetrominoType::O => {}
         _ => {
@@ -50,7 +66,7 @@ pub fn rotate(tetromino: &mut Tetromino, placed: &Vec<Option<Block>>) {
                 tetromino.mat = new_tetromino.mat;
                 tetromino.rot_index = (tetromino.rot_index + 1) % 4;
             } else {
-                let res = mat3_super_kick(&new_tetromino, placed);
+                let res = mat3_super_kick(&KICKS, &new_tetromino, placed);
                 match res {
                     Ok(new_pos) => {
                         tetromino.mat = new_tetromino.mat;
@@ -66,7 +82,7 @@ pub fn rotate(tetromino: &mut Tetromino, placed: &Vec<Option<Block>>) {
     }
 }
 
-const TRANSLATIONS: [Vec2; 16] = [
+const KICKS: [Vec2; 16] = [
     const_vec2!([-1., 0.]),
     const_vec2!([-1., 1.]),
     const_vec2!([0., -2.]),
@@ -84,15 +100,34 @@ const TRANSLATIONS: [Vec2; 16] = [
     const_vec2!([0., 2.]),
     const_vec2!([-1., 2.]),
 ];
+const I_KICKS: [Vec2; 16] = [
+    const_vec2!([-2., 0.]),
+    const_vec2!([1., 0.]),
+    const_vec2!([-2., -1.]),
+    const_vec2!([1., 2.]),
+    const_vec2!([-1., 0.]),
+    const_vec2!([2., 0.]),
+    const_vec2!([-1., 2.]),
+    const_vec2!([2., -1.]),
+    const_vec2!([2., 0.]),
+    const_vec2!([-1., 0.]),
+    const_vec2!([2., 1.]),
+    const_vec2!([-1., -2.]),
+    const_vec2!([1., 0.]),
+    const_vec2!([-2., 0.]),
+    const_vec2!([1., -2.]),
+    const_vec2!([-2., 1.]),
+];
 pub fn mat3_super_kick(
+    kicks: &[Vec2],
     tetromino: &Tetromino,
     placed: &Vec<Option<Block>>,
 ) -> Result<Vec2, &'static str> {
     for x in 0..4 {
         let idx = rel_xy_idx(x as f32, tetromino.rot_index as f32, 4.0);
         let new_pos = vec2(
-            &tetromino.pos.x + TRANSLATIONS[idx].x,
-            &tetromino.pos.y + TRANSLATIONS[idx].y,
+            &tetromino.pos.x + kicks[idx].x,
+            &tetromino.pos.y + kicks[idx].y,
         );
         if can_translate(tetromino, placed, &new_pos) {
             return Ok(new_pos);
